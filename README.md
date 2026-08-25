34aae588525697cd67d912aeaa6c3053657b6bbd
christianmz565 <christianmz565@gmail.com>
2026-08-25 15:59:32 -0500

    chore: init repo

---
+++ README.md
@@ -0,0 +1,2 @@
+> [!WARNING]
+> Esta es una aplicación en desarrollo, no se recomienda su uso hasta que se realize una publicación estable. Por favor, tenga en cuenta que la funcionalidad puede cambiar y que pueden existir errores.
---
+++ flake.nix
@@ -0,0 +1,63 @@
+
+{
+  description = "A flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-26.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      nixpkgs-unstable,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+        unstable = import nixpkgs-unstable {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          LD_LIBRARY_PATH =
+            with pkgs;
+            lib.makeLibraryPath [
+              stdenv.cc.cc
+              zlib
+              glib
+              libxcb
+              libglvnd
+            ];
+
+          packages = pkgs.lib.flatten [
+            (with pkgs; [
+              go
+            ])
+            (with unstable; [
+            ])
+          ];
+          env = {
+          	GOPROXY = "https://proxy.golang.org,direct";
+          	GOSUMDB = "sum.golang.org";
+          };
+          shellHook = ''
+            export GOPATH="$PWD/.go"
+            export GOBIN="$GOPATH/bin"
+            mkdir -p "$GOBIN"
+            export PATH="$GOBIN:$PATH"
+          '';
+          buildInputs = [ pkgs.bashInteractive ];
+        };
+      }
+    );
+}
